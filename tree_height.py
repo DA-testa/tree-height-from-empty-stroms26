ffc89e9389d5d96b54d5402d84b7067f4299a11e
Haralds Strombergs <121034489+stroms26@users.noreply.github.com>
2023-03-05 20:16:11 +0000

    t5

--- tree_height.py
+++ tree_height.py
@@ -2,61 +2,61 @@
 
 import sys
 import threading
-import numpy as np
+
 
 def compute_height(n, parents):
-    # adjacency list
-    adj_list = [[] for i in range(n)]
+    # adjacency saraksts
+    adj_list = [[] for _ in range(n)]
     for i in range(n):
         if parents[i] == -1:
             root = i
         else:
             adj_list[parents[i]].append(i)
 
-    # recursion
-    def dfs(node):
-        height = 0
+    # BFS to augstuma noteiksanai
+    max_height = 0
+    q = [(root, 1)]
+    while q:
+        node, height = q.pop(0)
+        if height > max_height:
+            max_height = height
         for child in adj_list[node]:
-            height = max(height, dfs(child))
-        return height + 1
+            q.append((child, height+1))
 
-    # calculate height
-    return dfs(root)
+    return max_height
 
 
 def main():
-    # get input type (I or F)
+    # (I vai F)
     input_type = input().strip().upper()
 
-    # handle keyboard input
+    #  keyboard input
     if input_type == 'I':
         n = int(input().strip())
-        parents = np.array(list(map(int, input().strip().split())))
+        parents = list(map(int, input().strip().split()))
         print(compute_height(n, parents))
 
-    # handle file input
+    # file input
     elif input_type == 'F':
         filename = input().strip()
         if 'a' not in filename:
             try:
                 with open(filename, 'r') as f:
                     n = int(f.readline().strip())
-                    parents = np.array(list(map(int, f.readline().strip().split())))
+                    parents = list(map(int, f.readline().strip().split()))
                     print(compute_height(n, parents))
             except FileNotFoundError:
                 print(f'Error: File {filename} not found')
         else:
-            print('Error: File name cannot contain letter "a"')
+            print('nedrikst saturet "a"')
 
-    # handle invalid input
+    # invalid input
     else:
-        print('Error: Invalid input')
+        print('nepareiza ievade')
 
 
-# increase recursion depth limit and thread stack size
+# rekursijas limits
 sys.setrecursionlimit(10**7)
 threading.stack_size(2**27)
 threading.Thread(target=main).start()
 
-# print test array
-print(np.array([1,2,3]))
