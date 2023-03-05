66286998b4ae582df1c2f9765aad940a5b37fd4b
Haralds Strombergs <121034489+stroms26@users.noreply.github.com>
2023-03-05 19:43:21 +0000

    test

--- tree_height.py
+++ tree_height.py
@@ -5,14 +5,46 @@ import threading
 import numpy
 
 
+
 def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
+    # adjacency list
+    adj_list = [[] for i in range(n)]
+    for i in range(n):
+        if parents[i] == -1:
+            root = i
+        else:
+            adj_list[parents[i]].append(i)
+
+    #rekursija
+    def dfs(node):
+        height = 0
+        for child in adj_list[node]:
+            height = max(height, dfs(child))
+        return height + 1
+
+    # aprekina augstumu
+    return dfs(root)
 
 
 def main():
+    # I vai F
+    input_type = input().strip().upper()
+    if input_type == 'I':
+        n = int(input().strip())
+        parents = np.array(list(map(int, input().strip().split())))
+        print(compute_height(n, parents))
+    elif input_type == 'F':
+        filename = input().strip()
+        if 'a' not in filename:
+            with open('test/' + filename, 'r') as f:
+                n = int(f.readline().strip())
+                parents = np.array(list(map(int, f.readline().strip().split())))
+                print(compute_height(n, parents))
+    else:
+        print('Invalid input')
+
+
+
     # implement input form keyboard and from files
     
     # let user input file name to use, don't allow file names with letter a
@@ -29,5 +61,5 @@ def main():
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
-main()
+#main()
 # print(numpy.array([1,2,3]))
