06e7e4ab34a0e9e22aef483ef6df7ddfb206052a
Haralds Strombergs <121034489+stroms26@users.noreply.github.com>
2023-03-05 20:02:49 +0000

    t4

--- tree_height.py
+++ tree_height.py
@@ -4,8 +4,6 @@ import sys
 import threading
 import numpy as np
 
-
-
 def compute_height(n, parents):
     # adjacency list
     adj_list = [[] for i in range(n)]
@@ -33,7 +31,7 @@ def main():
     # handle keyboard input
     if input_type == 'I':
         n = int(input().strip())
-        parents = list(map(int, input().strip().split()))
+        parents = np.array(list(map(int, input().strip().split())))
         print(compute_height(n, parents))
 
     # handle file input
@@ -43,7 +41,7 @@ def main():
             try:
                 with open(filename, 'r') as f:
                     n = int(f.readline().strip())
-                    parents = list(map(int, f.readline().strip().split()))
+                    parents = np.array(list(map(int, f.readline().strip().split())))
                     print(compute_height(n, parents))
             except FileNotFoundError:
                 print(f'Error: File {filename} not found')
@@ -55,21 +53,10 @@ def main():
         print('Error: Invalid input')
 
 
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    
-
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
+# increase recursion depth limit and thread stack size
+sys.setrecursionlimit(10**7)
+threading.stack_size(2**27)
 threading.Thread(target=main).start()
-#main()
+
+# print test array
 print(np.array([1,2,3]))
